d639376964d28174b1a3c7a9ccbc0e88c830396d
ShyroVO <bosquet.laurie@outlook.com>
2020-12-01 16:50:48 +0100

    0.1

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log(random());
+
+let entier = document.getElementById("entier");
+let flottant = document.getElementById("flottant");
+let nonNumerique = document.getElementById("non-numerique");
+let randome = document.getElementById("random");
+let trunc = document.getElementById("trunc");
+let replace = document.getElementById("replace");
+let operande = 10;
+
+entier.innerHTML= "text";
